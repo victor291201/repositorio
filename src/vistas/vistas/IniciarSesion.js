af14baaf8694654f6405f3189aee8917b345aca1
victor291201 <victor291201hugo@gmail.com>
2023-02-15 15:38:00 -0500

    first commit

---
+++ src/vistas/vistas/IniciarSesion.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../utileria/IniciarSesion.css"
+
+function IniciarSesion() {
+    return (
+      <div className="contenedor">
+        <div className="pt1"></div>
+        <div className="pt2">
+            <div className="formulario">
+                <div className="igd"></div>
+                <div className="usuario">
+                    <label className="usLabel" htmlFor="usLabel">Usuario</label>
+                    <input type="text" className="usInput" id="usLabel"/>
+                </div>
+                <div className="contraseña">
+                    <label className="usLabel" htmlFor="usLabel">Contraseña</label>
+                    <input type="password" className="usInput" id="usLabel"/>
+                </div>
+                <div className="adicionales">
+                    <div className="recordarme">
+                        <input type="checkbox" id="recordarme"/>
+                        <label htmlFor="recordarme">Recordarme mas tarde</label>
+                    </div>
+                    <a>Olvide mi contraseña</a>
+                </div>
+                <button>Iniciar iniciarSesion</button>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default IniciarSesion;
+  
